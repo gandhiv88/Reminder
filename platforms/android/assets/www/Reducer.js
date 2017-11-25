a4ae20969566f48a02ff2b57a7197703393c3387
Valliappan <GANDHI_VALLIAPPAN@homedepot.com>
2017-11-25 00:15:21 -0500

    cordova buils issue resolved

--- platforms/android/assets/www/Reducer.js
+++
@@ -1,5 +0,0 @@
-import {combineReducers} from "redux";
-import createReducer from "./app/reducer/createReducer";
-export default combineReducers({
-  createReducer
-});
--- platforms/android/assets/www/store.js
+++
@@ -1,6 +0,0 @@
-import {createStore,applyMiddleware} from "redux";
-import logger from "redux-logger";
-
-import reducer from "./Reducer";
-
-export const store = createStore(reducer,{}, applyMiddleware(logger));
